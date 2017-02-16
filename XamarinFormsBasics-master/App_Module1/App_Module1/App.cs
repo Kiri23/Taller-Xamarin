dd6106e32f24a3668195e61eae8eff2fdb159763
Victor Olivo <VictorJosueOlivo@Victors-MacBook-Pro.local>
2017-02-16 14:48:29 -0400

    SetUp Porject

---
+++ XamarinFormsBasics-master/App_Module1/App_Module1/App.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime;
+using App_Module1.Views;
+
+using Xamarin.Forms;
+
+namespace App_Module1
+{
+    public class App : Application
+    {
+        public App()
+        {
+
+            Button button = new Button
+            {
+                Text = "Next Page",
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+
+            };
+
+            // The root page of your application
+            var content = new ContentPage
+            {
+                Title = "App_Module1",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!",
+                            VerticalOptions = LayoutOptions.CenterAndExpand
+
+                        },
+                        
+                        button
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+
+            //Initializes 'Clicked' event for button and points to the desired method
+            button.Clicked +=  delegate 
+           {
+               //PASSING DATA
+               //Pass through new page objects constructor
+               content.Navigation.PushAsync(new LayoutPage1(DateTime.Now.ToString()));
+           };
+
+            //Tambien podria ser 
+            //button.Clicked += OnButtonClicked;
+        }
+
+        
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+            
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+
+        //private void OnButtonClicked(object sender, EventArgs e)
+        //{
+          
+        //    // In the PCL Project this is the way to print lines or objects to the application output
+        //    System.Diagnostics.Debug.WriteLine("Button Clicked Method");
+            
+
+        //}
+
+
+    }
+}
---
+++ XamarinFormsBasics-master/App_Module1/App_Module1/Views/LayoutPage2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App_Module1.Views
+{
+    public partial class LayoutPage2 : ContentPage
+    {
+        public LayoutPage2()
+        {
+            InitializeComponent();
+
+            nextButton.Clicked += nextPageclick;
+
+        }
+
+        private async void nextPageclick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ScrollViewPage());
+        }
+
+        protected override async void OnAppearing()
+        {
+            redBox.Opacity = 0;
+            blueBox.Opacity = 0;
+            await Task.WhenAll(
+                redBox.FadeTo(1, 2000, Easing.CubicIn),
+                blueBox.FadeTo(1, 3000)
+            );
+           
+        }
+    }
+}
